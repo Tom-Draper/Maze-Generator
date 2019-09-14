dbd30957094e4120b12c2537acaf6e7bfa8dbae5
tomdraper <tomjdraper1@gmail.com>
2019-09-14 17:35:07 +0100

    Remove vertex

--- MazeGenerator/tree.py
+++ MazeGenerator/tree.py
@@ -6,8 +6,15 @@ class Tree():
         self.vertices = []
 
     def addVertex(self, id, value):
-        if vertices.contains("vertex{0}".format(id)):
+        if self.vertices.contains("vertex{0}".format(id)):
             print('Vertex ID already exists.')
         else:
             self.vertices.append({"vertex{0}".format(id): value})
+            
+    def removeVertex(self, id, value):
+        if self.vertices.contains("vertex{0}".format(id)):
+            self.vertices.remove("vertiex{0}".format(id))
+            print('Vertex removed.')
+        else:
+            print('Vertex doesn\'t exist.')
     
