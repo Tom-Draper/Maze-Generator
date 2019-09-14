7d167194c108a5237b7b6065e4fa96330b3399b6
tomdraper <tomjdraper1@gmail.com>
2019-09-14 20:51:02 +0100

    Generate maze tree

--- MazeGenerator/mazeGenerator.py
+++ MazeGenerator/mazeGenerator.py
@@ -1,9 +1,76 @@
 from tree import Tree
+import random
 
-tree = Tree()
+class MazeGenerator():
+    
+    def __init__(self, size):
+        self.tree = Tree()
+        self.usedWeights = []
+        
+        self.createTree(size)
+        self.initConnections(size)
+        
+    def createTree(self, size):
+        # Create size squared vertices 
+        for vertexID in range(size * size):
+            self.tree.addVertex(vertexID)
+            
+    def getVertexAbove(self, size, vertexID):
+        # If vertex in top row
+        if vertexID < size:
+            return -1
+        else:
+            return vertexID - size
+            
+    def getVertexBelow(self, size, vertexID):
+        # If vertex in bottom row
+        if vertexID >= (size * (size - 1)):
+            return -1
+        else:
+            return vertexID + size
+        
+    def getVertexLeft(self, size, vertexID):
+        # If vertex in left column
+        if (vertexID + size) % size == 0:
+            return -1
+        else:
+            return vertexID - 1
+        
+    def getVertexRight(self, size, vertexID):
+        # If vertex in left column
+        if (vertexID + 1) % size == 0:
+            return -1
+        else:
+            return vertexID + 1
+            
+    def initConnections(self, size):
+        for vertexID in range(size*size):
+            left = self.getVertexLeft(size, vertexID)
+            right = self.getVertexRight(size, vertexID)
+            above = self.getVertexAbove(size, vertexID)
+            below = self.getVertexBelow(size, vertexID)
+            
+            if left != -1 and not self.tree.connectionExist(vertexID, left):
+                self.tree.addWeightedConnection(vertexID, left, self.getNewWeight(size))
+            if right != -1 and not self.tree.connectionExist(vertexID, right):
+                self.tree.addWeightedConnection(vertexID, right, self.getNewWeight(size))
+            if above != -1 and not self.tree.connectionExist(vertexID, above):
+                self.tree.addWeightedConnection(vertexID, above, self.getNewWeight(size))
+            if below != -1 and not self.tree.connectionExist(vertexID, below):
+                self.tree.addWeightedConnection(vertexID, below, self.getNewWeight(size))
+            
+                
+    def getNewWeight(self, size):
+        maxWeight = ((size-1) * size * 2) - 1 # Number of connections
+        
+        weight = random.randint(0, maxWeight)
+        while weight in self.usedWeights:
+            weight = random.randint(0, maxWeight)
+            
+        self.usedWeights.append(weight)
+            
+        return weight
+    
+generator = MazeGenerator(5)
+generator.tree.displayTree()
 
-tree.addVertex(0)
-tree.addVertex(0)
-tree.addVertex(1)
-tree.addConnection(0, 1, 4)
-tree.displayTree()
--- MazeGenerator/tree.py
+++ MazeGenerator/tree.py
@@ -3,38 +3,73 @@ from vertex import Vertex
 class Tree():
     
     def __init__(self):
-        self.vertices = {}  # Example: {0 : {1:4, 5:4}, 1 : {0:4, 2:6, 6:3}, ...}
+        self.vertices = {}  # Example: {0(ID) : {1(vertexID):4(weight), 5(vertexID):4(weight)}, 
+                            #           1 : {0:4, 2:6, 6:3}, ...}
 
-    def addVertex(self, id):
-        if id in self.vertices.keys():
+    def addVertex(self, vertexID):
+        # If vertex id not found, add vertex to vertices dict
+        if vertexID in self.vertices.keys():
             print('Vertex ID already exists.')
         else:
-            self.vertices[id] = {} # Add id with no connections
+            self.vertices[vertexID] = {} # Add id with no connections
             
-    def removeVertex(self, id):
-        if id in self.vertices.keys():
-            del self.vertices[id]
+    def removeVertex(self, vertexID):
+        # If vertex id found, remove vertex from vertices dict
+        if vertexID in self.vertices.keys():
+            del self.vertices[vertexID]
             print('Vertex removed.')
         else:
-            print('Vertex ' + str(id) + ' not found.')
+            print('Vertex ' + str(vertexID) + ' not found.')
             
-    def addConnection(self, firstId, secondId, weight):
+    def addConnection(self, firstId, secondId):
+        # If both vertices exist, add connection between the two
         if firstId in self.vertices.keys() and secondId in self.vertices.keys():
-            self.vertices[firstId][secondId] = weight
-            self.vertices[secondId][firstId] = weight
-            print('Added weight ' + str(weight) + ' between vertex ' 
-                  + str(firstId) + ' and ' + str(secondId) + '.')
+            # If no connection exists, add connection
+            if not secondId in self.vertices[firstId]:
+                self.vertices[firstId][secondId] = 1
+                self.vertices[secondId][firstId] = 1
+                print('Added connection between vertex ' + str(firstId) + ' and ' 
+                      + str(secondId) + '.')
+            else:
+                print('There is already a connection between vertex ' + 
+                      str(firstId) + ' and ' + str(secondId))
+        else:
+            print('Vertex not found.')
+            
+    def addWeightedConnection(self, firstId, secondId, weight):
+        # If both vertices exist, add connection between the two
+        if firstId in self.vertices.keys() and secondId in self.vertices.keys():
+            # If no connection exists, add connection
+            if not secondId in self.vertices[firstId]:
+                self.vertices[firstId][secondId] = weight
+                self.vertices[secondId][firstId] = weight
+                print('Added weight ' + str(weight) + ' between vertex ' 
+                    + str(firstId) + ' and ' + str(secondId) + '.')
+            else:
+                print('There is already a connection between vertex ' + 
+                      str(firstId) + ' and ' + str(secondId))
         else:
             print('Vertex not found.')
             
     def removeConnection(self, firstId, secondId):
-        if firstId and secondId in self.vertices.keys():
-            del self.vertices[firstId][secondId]
-            del self.vertices[secondId][firstId]
-            print('Removed connection between vertex ' + str(firstId) + ' and ' 
-                  + str(secondId) + '.')
+        # If both vertices exist, remove connection between the two
+        if firstId in self.vertices.keys() and secondId in self.vertices.keys():
+            # If connection exists, remove connection
+            if secondId in self.vertices[firstId]:
+                del self.vertices[firstId][secondId]
+                del self.vertices[secondId][firstId]
+                print('Removed connection between vertex ' + str(firstId) + ' and ' 
+                      + str(secondId) + '.')
+            else:
+                print('Connection doesn\'t exist between vertex ' + str(firstId)
+                      + str(secondId) + '.')
         else:
             print('Vertex not found.')
+            
+    def connectionExist(self, firstId, secondId):
+        if (firstId in self.vertices.keys() and secondId in self.vertices.keys() and 
+                secondId in self.vertices[firstId]):
+            return True
             
     def displayTree(self):
         # Check if dictionary empty
@@ -43,9 +78,10 @@ class Tree():
         else:
             print('No vertices found.')
         
-        # Print each vertex id and its connections
-        for id in self.vertices.keys():
-            connections = self.vertices[id]
-            print(str(id) + ': ' + str(connections))
+        # Print each vertex idand its connections
+        for vertexID in self.vertices.keys():
+            connections = self.vertices[vertexID]
+            print(str(vertexID) + ': ' + str(connections))
+
 
     
