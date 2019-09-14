f95937832a515f0b65f79619bd872805d06cc10e
tomdraper <tomjdraper1@gmail.com>
2019-09-14 17:31:58 +0100

    Start tree structure

---
+++ MazeGenerator/tree.py
@@ -0,0 +1,13 @@
+from vertex import Vertex
+
+class Tree():
+    
+    def __init__(self):
+        self.vertices = []
+
+    def addVertex(self, id, value):
+        if vertices.contains("vertex{0}".format(id)):
+            print('Vertex ID already exists.')
+        else:
+            self.vertices.append({"vertex{0}".format(id): value})
+    
